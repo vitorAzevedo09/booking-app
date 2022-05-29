1146676a504c5ee616385ba4469e89c04cc1a8d0
Vitor A. Silva <vitor.azevedo009@gmail.com>
2022-05-29 14:12:55 -0300

    some shit

--- models/entities.go
+++ models/entities.go
@@ -1,5 +1,8 @@
 package models
 
 type Todo struct {
-	ID int64
+	ID          int64  `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Done        bool   `json:"done"`
 }
